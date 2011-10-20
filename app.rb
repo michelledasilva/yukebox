574f65000c8fbc6603bdbd934a671a174a345038
Michelle Da Silva <michelle@empact.co.za>
2011-10-20 22:22:16 +0200

    proper json back from app, basic search results rendering

--- app.rb
+++ app.rb
@@ -10,6 +10,7 @@ class App < Sinatra::Base
   get "/search" do
     #"<h1>#{params[:q]}</h1>"
     
+    content_type "application/json"
     Youtube.search( params[:q] ).to_json
   end
   
--- public/js/script.js
+++ public/js/script.js
@@ -4,8 +4,10 @@
 
 $(document).ready( function() {
   $("#search_btn").click(function() {
+    $('#results').html("<em>Searching...</em>");
+    
     var q = $("#search").val();
-    var u = "/search?q=" + $.param(q);
+    var u = "/search?q=" + encodeURIComponent(q);
     $.ajax({
       url: u,
       type: 'GET',
@@ -17,6 +19,16 @@ $(document).ready( function() {
 
 function showSearchResults(data){
   console.log(data);
+  var list = $("<ul></ul>");
+  
+  for( var i = 0; i < data.length; i++ ) {
+    var video = data[i];
+    
+    var r = $("<li>" + video.title + " (" + video.length + ")</li>");
+    list.append( r )
+  }
+  
+  $('#results').html( list );
 }
 
 
