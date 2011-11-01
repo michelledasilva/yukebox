45b12c036c0629080449e6916942a1a0ba65c14a
Michelle Da Silva <michelle@empact.co.za>
2011-11-01 20:17:08 +0200

    Going purple

--- public/js/script.js
+++ public/js/script.js
@@ -47,7 +47,7 @@ function showSearchResults(data){
   for( var i = 0; i < data.length; i++ ) {
     var video = data[i];
     
-    var r = $("<li class=\"result-video\" data-vid=\"" + video.id +"\" data-url=\"" + video.url + "\" data-type=\"" + video.type + "\">" + video.title + " (" + video.length + ")</li>");
+    var r = $("<li class=\"result-video plus\" data-vid=\"" + video.id +"\" data-url=\"" + video.url + "\" data-type=\"" + video.type + "\">" + video.title + " (" + video.length + ")</li>");
     list.append( r )
   }
   
